8cc20bb31daab4f8fc98b27c9cc69a654f69b311
Brian Heuston <bah5069@gmail.com>
2014-11-28 22:16:01 -0800

    Initial commit of V2 using Unity 4.6

---
+++ Assets/Resources/Scripts/MainMenuHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuHandler : MonoBehaviour {
+    enum MenuState
+    {
+        Normal,
+        Quit
+    }
+    private MenuState CurrentState;
+
+    private GameObject mainMenu;
+    private GameObject quitMenu;
+
+	// Use this for initialization
+	void Start () {
+        CurrentState = MenuState.Normal;
+        mainMenu = GameObject.Find("MainMenu");
+        quitMenu = GameObject.Find("QuitGameMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (CurrentState)
+        {
+            case MenuState.Normal:
+                mainMenu.SetActive(true);
+                quitMenu.SetActive(false);
+                break;
+            case MenuState.Quit:
+                mainMenu.SetActive(false);
+                quitMenu.SetActive(true);
+                break;
+        }
+	}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CancelQuitGame()
+    {
+        CurrentState = MenuState.Normal;
+    }
+
+    public void CreateQuitGamePopup()
+    {
+        CurrentState = MenuState.Quit;
+    }
+}
---
+++ Assets/Resources/Scripts/TimeController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/////////////////////////////////////////////////////////////////////
+// Class TimeController
+// 
+// Usage:
+//     Attach to a persistent GameObject in a scene
+/////////////////////////////////////////////////////////////////////
+public class TimeController : MonoBehaviour {
+ 
+    public static float deltaTime;
+    private static float _lastframetime;
+	void Start () {
+        _lastframetime = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deltaTime = Time.realtimeSinceStartup - _lastframetime;
+        _lastframetime = Time.realtimeSinceStartup;
+	}
+}
