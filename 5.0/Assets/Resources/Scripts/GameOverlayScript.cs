fb3c2bc61388897508506ca5a53dae76a9f98a07
Brian Heuston <bah5069@gmail.com>
2015-03-07 19:19:24 -0800

    Make 5.0 version, move old to 4.6. Also remove all plugins as most are broken currently.

---
+++ 5.0/Assets/Resources/Scripts/GameOverlayScript.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverlayScript : MonoBehaviour {
+    enum MenuState {
+        GAME,
+        PAUSE,
+        OPTIONS,
+        QUIT
+    }
+
+    private MenuState currentState;
+    private MenuState CurrentState {
+        get
+        {
+            return currentState;
+        }
+        set {
+            currentState = value;
+
+            switch (currentState)
+            {
+                case MenuState.GAME:
+                    GameState();
+                    break;
+                case MenuState.PAUSE:
+                    PauseState();
+                    break;
+                case MenuState.OPTIONS:
+                    OptionsState();
+                    break;
+                case MenuState.QUIT:
+                    QuitState();
+                    break;
+            }
+        }
+    }
+
+    private float fOldTimeScale;
+
+    private string szPauseMenuName = "PauseCanvas";
+    private string szQuitMenuName = "QuitCanvas";
+    
+    private GameObject oPauseMenu;
+    private GameObject oQuitMenu;
+
+	// Use this for initialization
+	void Start () {
+        oPauseMenu = GameObject.Find(szPauseMenuName);
+        oQuitMenu = GameObject.Find(szQuitMenuName);
+
+        oPauseMenu.SetActive(false);
+        oQuitMenu.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void FixedUpdate()
+    {
+    }
+
+    #region GameStateFunctions
+    private void GameState()
+    {
+        oPauseMenu.SetActive(false);
+        oQuitMenu.SetActive(false);
+        Time.timeScale = fOldTimeScale;
+    }
+
+    private void QuitState()
+    {
+        oPauseMenu.SetActive(false);
+        oQuitMenu.SetActive(true);
+    }
+
+    private void PauseState()
+    {
+        fOldTimeScale = Time.timeScale;
+        Time.timeScale = 0.0f;
+
+        oPauseMenu.SetActive(true);
+        oQuitMenu.SetActive(false);
+    }
+
+    private void OptionsState()
+    {
+
+    }
+    #endregion
+
+    #region MenuFunctions
+    public void PauseGame()
+    {
+        CurrentState = MenuState.PAUSE;
+    }
+
+    public void ResumeGame()
+    {
+        CurrentState = MenuState.GAME;
+    }
+
+    public void QuitPrompt()
+    {
+        CurrentState = MenuState.QUIT;
+    }
+
+    public void QuitCancel()
+    {
+        CurrentState = MenuState.PAUSE;
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = fOldTimeScale;
+        Application.LoadLevel("MainMenuScene");
+    }
+    #endregion
+}
---
+++ 5.0/Assets/Resources/Scripts/MainMenuHandler.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuHandler : MonoBehaviour {
+    enum MenuState
+    {
+        Normal,
+        Quit
+    }
+    private MenuState currentState;
+    private MenuState CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+        set
+        {
+            currentState = value;
+
+            switch (currentState)
+            {
+                case MenuState.Normal:
+                    NormalState();
+                    break;
+                case MenuState.Quit:
+                    QuitState();
+                    break;
+            }
+        }
+    }
+
+    private GameObject mainMenu;
+    private GameObject quitMenu;
+
+	// Use this for initialization
+	void Start () {
+        mainMenu = GameObject.Find("MainMenuCanvas");
+        quitMenu = GameObject.Find("QuitCanvas");
+
+        CurrentState = MenuState.Normal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CancelQuitGame()
+    {
+        CurrentState = MenuState.Normal;
+    }
+
+    public void CreateQuitGamePopup()
+    {
+        CurrentState = MenuState.Quit;
+    }
+
+    public void NewGame()
+    {
+        Application.LoadLevel("GameScene");
+    }
+
+    #region STATE_FUNCTIONS
+    private void NormalState()
+    {
+        mainMenu.SetActive(true);
+        quitMenu.SetActive(false);
+    }
+
+    private void QuitState()
+    {
+        mainMenu.SetActive(false);
+        quitMenu.SetActive(true);
+    }
+    #endregion
+}
